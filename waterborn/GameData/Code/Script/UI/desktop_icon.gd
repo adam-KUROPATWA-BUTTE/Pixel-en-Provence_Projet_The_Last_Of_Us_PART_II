ff8e74ab99a8f27eccb8e3e981d16f3fc6ad6584
Kensufox <163044607+QUILLIEC-Matisse-24019562@users.noreply.github.com>
2026-04-14 20:14:52 +0200

    more

---
+++ waterborn/GameData/Code/Script/UI/desktop_icon.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_icon_hit_box_pressed() -> void:
+	print("icon clicked")
--- waterborn/GameData/Code/Script/UI/window.gd
+++ waterborn/GameData/Code/Script/UI/window.gd
@@ -2,7 +2,11 @@ extends NinePatchRect
 
 @export var window_size : Vector2i = Vector2i(100, 70)
 @export var pos : Vector2i = Vector2i(150, 180)
+@export var window_name : String = "Text"
+
+@onready var name_label : Label = $WindowTopBar/WindowName
 
 func _ready() -> void:
 	size = window_size
 	position = pos
+	name_label.text = window_name
