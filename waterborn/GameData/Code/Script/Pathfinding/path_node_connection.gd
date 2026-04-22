ff772cb8d0b1f75846d02736bbaa87e10cd8e60c
Kensufox <163044607+QUILLIEC-Matisse-24019562@users.noreply.github.com>
2026-04-22 14:33:07 +0200

    import tiny world pathfinding and make it 3d

--- waterborn/GameData/Code/Script/Pathfinding/path_node_connection.gd
+++ waterborn/GameData/Code/Script/Pathfinding/path_node_connection.gd
@@ -1,15 +1,23 @@
 extends Node3D
 
+## Passive Pathfinding var
 @export var node_connection_distance : float = 6
-@onready var threshold_square : float = node_connection_distance * node_connection_distance
 
 var connections : Array = []
 var time_to_process : float
+var nodes : Array[Node]
+
+## Active Pathfinding var
+var astar: AStar3D = AStar3D.new()
+var start : Vector3
+var target : Vector3
 
 func _ready() -> void:
 	var start_time = Time.get_ticks_usec()
 	print("path node connection ready")
-	var nodes : Array[Node] = get_children()
+	nodes = get_children()
+	
+	var threshold_square : float = node_connection_distance * node_connection_distance
 	
 	var node_index = {}
 	for i in range(nodes.size()):
@@ -54,4 +62,77 @@ func _ready() -> void:
 	print("connections : ", connections.size())
 	var end_time = Time.get_ticks_usec()
 	time_to_process = (end_time - start_time) / 1_000_000.0
+	build_astar_grid()
 	print("time to process pathfinding nodes = ", time_to_process, "s")
+	print(astar)
+
+#func _draw() -> void:
+	#if astar == null and (path.size() == 0 or path == null or path.is_empty() and !debug):
+		#return
+#
+	#if (path.size() == 0 or path == null or path.is_empty()):
+		#for id in astar.get_point_ids():
+			#var connections = astar.get_point_connections(id)
+			##if debug:
+				##for lid in connections:
+					##draw_line(
+						##Vector2(
+						##(astar.get_point_position(id).x*cell_size), 
+						##(astar.get_point_position(id).y*cell_size)), 
+						##Vector2(
+						##(astar.get_point_position(lid).x*cell_size), 
+						##(astar.get_point_position(lid).y*cell_size)), 
+						 ##Color.WHITE, 2.0)
+		#for id in astar.get_point_ids():
+			#var pos = Vector2(
+					#(astar.get_point_position(id).x*cell_size), 
+					#(astar.get_point_position(id).y*cell_size))
+			##if debug:
+				##draw_circle(pos, 4.0, Color.BLUE)
+#
+	## Draw current path in red
+	##if debug:
+		##for point in path:
+			##draw_circle(point, 4.0, Color.RED)
+		##for i in range(path.size() - 1):
+			##draw_line(path[i], path[i + 1], Color.RED, 2.0)	
+		##if start:
+			##draw_circle(start, 4.0, Color.YELLOW)
+		##if target:
+			##draw_circle(target, 4.0, Color.GREEN)
+
+func build_astar_grid():
+	for connection in connections:
+		if !astar.has_point(nodes.find(connection[0])):
+			astar.add_point(nodes.find(connection[0]), connection[0].position)
+		if !astar.has_point(nodes.find(connection[1])):
+			astar.add_point(nodes.find(connection[1]), connection[1].position)
+		astar.connect_points(nodes.find(connection[0]), nodes.find(connection[1]), true)
+
+func get_astar_path(imported_start: Vector3, imported_target: Vector3) -> PackedVector3Array:
+	var sid : int = astar.get_closest_point(imported_start)
+	var tid : int = astar.get_closest_point(imported_target)
+	# Validate AStar points before accessing their positions
+	if not astar.has_point(sid) or not astar.has_point(tid):
+		print("Invalid start or target point")
+		return PackedVector3Array()
+
+	start = Vector3(astar.get_point_position(sid))
+	target = Vector3(astar.get_point_position(tid))
+	if astar.get_point_connections(sid).is_empty():
+		print("Start point has no connections")
+		return PackedVector3Array()
+	if astar.get_point_connections(tid).is_empty():
+		print("Target point has no connections")
+		return PackedVector3Array()
+
+	var raw_path : PackedVector3Array = astar.get_point_path(sid, tid)
+
+	if raw_path.size() < 1:
+		print("Path too short: ", raw_path)
+		return PackedVector3Array()
+
+	var scaled_path : PackedVector3Array = PackedVector3Array()
+	for path in raw_path:
+		scaled_path.append(path)
+	return scaled_path
