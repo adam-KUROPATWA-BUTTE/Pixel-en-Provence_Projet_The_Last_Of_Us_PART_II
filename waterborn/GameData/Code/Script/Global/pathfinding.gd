55f50381000909dc5ba47ed65c2045c1855563ba
Kensufox <163044607+QUILLIEC-Matisse-24019562@users.noreply.github.com>
2026-05-16 18:06:16 +0200

    entry nodes

--- waterborn/GameData/Code/Script/Global/pathfinding.gd
+++ waterborn/GameData/Code/Script/Global/pathfinding.gd
@@ -6,6 +6,23 @@ var astar: AStar3D = AStar3D.new()
 var start : Vector3
 var target : Vector3
 
+var entry_nodes : Array
+var node_threshold : float = 2.8 * 2.8
+
+func connect_room_by_entry() -> void:
+	for entry_node in entry_nodes:
+		for exit_node in entry_nodes:
+			if (
+				global_calculation.two_3d_position_to_distance_squared(entry_node.position, 
+				exit_node.position) <= node_threshold):
+				if !astar.has_point(entry_node):
+					astar.add_point(entry_node,entry_node.position)
+				if !astar.has_point(exit_node):
+					astar.add_point(exit_node,exit_node.position)
+				astar.connect_points(entry_node, exit_node, true)
+				entry_nodes.erase(entry_node)
+				entry_nodes.erase(exit_node)
+
 func get_astar_path(imported_start: Vector3, imported_target: Vector3) -> PackedVector3Array:
 	if !astar_ready:
 		print("astar path not ready, try again later")
