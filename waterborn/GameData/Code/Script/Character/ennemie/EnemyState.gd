49ec1155a4aa44b53cec25663c247c5abff7e158
Kensufox <163044607+QUILLIEC-Matisse-24019562@users.noreply.github.com>
2026-05-17 13:13:29 +0200

    some small fixing

--- waterborn/GameData/Code/Script/Character/ennemie/EnemyState.gd
+++ waterborn/GameData/Code/Script/Character/ennemie/EnemyState.gd
@@ -7,9 +7,12 @@ signal requested_transition_to_other_state(target_state_name: String, data: Dict
 
 func enter(previous_state_name: String, data := {}) -> void:
 	pass
+
 func exit() -> void:
 	pass
+
 func update(_delta: float) -> void:
 	pass
+
 func physics_update(_delta: float) -> void:
 	pass
--- waterborn/GameData/Code/Script/Element/valve.gd
+++ waterborn/GameData/Code/Script/Element/valve.gd
@@ -2,6 +2,8 @@ extends StaticBody3D
 
 @export var zone_index: int = 0
 var is_open: bool = false
+
 func interact():
 	is_open = !is_open
 	SystemeValve.valve_open[zone_index] = is_open
+	print("valve :", self, " is now open")
