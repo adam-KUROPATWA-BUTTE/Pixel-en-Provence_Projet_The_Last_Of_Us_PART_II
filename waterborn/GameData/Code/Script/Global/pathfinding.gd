3e5a70895e0ee21cc5bd68c94c35a678e7e2d067
QUILLIEC-Matisse-24019562 <163044607+QUILLIEC-Matisse-24019562@users.noreply.github.com>
2026-05-21 13:00:12 +0200

    fix path room connection bug

--- waterborn/GameData/Code/Script/Global/pathfinding.gd
+++ waterborn/GameData/Code/Script/Global/pathfinding.gd
@@ -1,27 +1,83 @@
 extends Node
 
 ## Active Pathfinding var 
+var in_process : Array = []
 var astar_ready : bool = false
 var astar: AStar3D = AStar3D.new()
 var start : Vector3
 var target : Vector3
 
 var entry_nodes : Array
-var node_threshold : float = 2.8 * 2.8
+var node_threshold : float = 2.8 ** 2
+
+var start_time
+var printed : bool = false
+
+func _process(_delta: float) -> void:
+	if start_time != 0 and !printed and astar_ready:
+		print((Time.get_ticks_usec() - start_time) / 1_000_000.0)
+		printed = true
+
+func add_astar(new_astar : AStar3D) -> void:
+	astar_ready = false
+	if start_time == null:
+		start_time = Time.get_ticks_usec()
+	var id = Time.get_ticks_usec()
+	in_process.append(id)
+	if astar.get_point_count() == 0:
+		astar = new_astar
+	else:
+		for point_id in new_astar.get_point_ids():
+			if not astar.has_point(point_id):
+				astar.add_point(point_id, new_astar.get_point_position(point_id))
+				astar.set_point_weight_scale(point_id, new_astar.get_point_weight_scale(point_id))
+		for point_id in new_astar.get_point_ids():
+			for neighbor_id in new_astar.get_point_connections(point_id):
+				if not astar.are_points_connected(point_id, neighbor_id):
+					astar.connect_points(point_id, neighbor_id)
+	
+	in_process.erase(id)
+	#print(in_process)
+	#print("amount remaining at add_astar : ", in_process.size() + entry_nodes.size())
+	if in_process.size() + entry_nodes.size() == 0:
+		astar_ready = true
+	connect_room_by_entry()
 
 func connect_room_by_entry() -> void:
+	var connected : Array = []
+	
 	for entry_node in entry_nodes:
+		var best_match = null
+		var best_distance : float = INF
 		for exit_node in entry_nodes:
-			if (
-				global_calculation.two_3d_position_to_distance_squared(entry_node.position, 
-				exit_node.position) <= node_threshold):
-				if !astar.has_point(entry_node):
-					astar.add_point(entry_node,entry_node.position)
-				if !astar.has_point(exit_node):
-					astar.add_point(exit_node,exit_node.position)
-				astar.connect_points(entry_node, exit_node, true)
-				entry_nodes.erase(entry_node)
-				entry_nodes.erase(exit_node)
+			if entry_node == exit_node or entry_node[2] == exit_node[2] or connected.has(entry_node) or connected.has(exit_node):
+				continue
+			var distance = global_calculation.two_3d_position_to_distance_squared(entry_node[0].global_position, exit_node[0].global_position)
+			
+			if distance <= node_threshold and distance < best_distance:
+				best_distance = distance
+				best_match = exit_node
+		if best_match != null:
+			if !astar.has_point(entry_node[1]):
+				astar.add_point(entry_node[1],entry_node[0].global_position)
+			if !astar.has_point(best_match[1]):
+				astar.add_point(best_match[1],best_match[0].global_position)
+			astar.connect_points(entry_node[1], best_match[1], true)
+			
+			if !connected.has(entry_node):
+				connected.append(entry_node)
+			if !connected.has(best_match):
+				connected.append(best_match)
+	
+	for node in connected:
+		entry_nodes.erase(node)
+	#print(entry_nodes)
+	#for node in entry_nodes:
+		#print("node position : ", node[0].global_position)
+	
+	#print("amount remaining at connect_entry : ", in_process.size() + entry_nodes.size())
+	if in_process.size() + entry_nodes.size() == 0:
+		astar_ready = true
 
 func get_astar_path(imported_start: Vector3, imported_target: Vector3) -> PackedVector3Array:
 	if !astar_ready:
--- waterborn/GameData/Code/Script/Pathfinding/path_node_connection.gd
+++ waterborn/GameData/Code/Script/Pathfinding/path_node_connection.gd
@@ -3,6 +3,7 @@ extends Node3D
 ## Passive Pathfinding var
 @export var node_connection_distance : float = 2.5
 
+var global_id : int
 var connections : Array = []
 var time_to_process : float
 var nodes : Array[Node]
@@ -11,13 +12,14 @@ var nodes : Array[Node]
 var astar: AStar3D = AStar3D.new()
 
 func _ready() -> void:
+	global_id = str(self).to_int()
+	
 	var start_time = Time.get_ticks_usec()
-	print("path node connection ready")
 	nodes = get_children()
 	
-	var threshold_square : float = node_connection_distance * node_connection_distance
+	var threshold_square : float = node_connection_distance ** 2
 	
-	var node_index = {}
+	var node_index : Dictionary = {}
 	for i in range(nodes.size()):
 		node_index[nodes[i]] = i
 	
@@ -26,16 +28,25 @@ func _ready() -> void:
 	
 	for node in nodes:
 		if node.entry:
-			pathfinding.entry_nodes.append(node)
+			pathfinding.entry_nodes.append([node, nodes.find(node)+global_id, global_id])
 		var cell = Vector3i(
-		floori(node.position.x / node_connection_distance),
-		floori(node.position.y / node_connection_distance),
-		floori(node.position.z / node_connection_distance)
+		floori(node.global_position.x / node_connection_distance),
+		floori(node.global_position.y / node_connection_distance),
+		floori(node.global_position.z / node_connection_distance)
 	)
 		if not grid.has(cell):
 			grid[cell] = []
 		grid[cell].append(node)
 	
+	grid_to_connections(grid, node_index, seen, threshold_square)
+	
+	#print("connections : ", connections.size())
+	var end_time = Time.get_ticks_usec()
+	time_to_process = (end_time - start_time) / 1_000_000.0
+	await build_astar_grid()
+	#print("time to process pathfinding nodes = ", time_to_process, "s")
+
+func grid_to_connections(grid : Dictionary, node_index : Dictionary, seen : Dictionary, threshold_square : float):
 	for cell in grid.keys():
 		for dx in [-1, 0, 1]:
 			for dy in [-1, 0, 1]:
@@ -55,23 +66,17 @@ func _ready() -> void:
 								continue
 							seen[key] = true
 							
-							var diff = node.position - check_node.position
-							if diff.dot(diff) <= threshold_square:
+							#var diff = global_calculation.two_3d_position_to_distance_squared(node.global_position, check_node.global_position)
+							if global_calculation.two_3d_position_to_distance_squared(node.global_position, check_node.global_position) <= threshold_square:
 								connections.append([node, check_node])
-	
-	print("connections : ", connections.size())
-	var end_time = Time.get_ticks_usec()
-	time_to_process = (end_time - start_time) / 1_000_000.0
-	await build_astar_grid()
-	print("time to process pathfinding nodes = ", time_to_process, "s")
-	print(astar)
 
 func build_astar_grid():
 	for connection in connections:
-		if !astar.has_point(nodes.find(connection[0])):
-			astar.add_point(nodes.find(connection[0]), connection[0].position)
-		if !astar.has_point(nodes.find(connection[1])):
-			astar.add_point(nodes.find(connection[1]), connection[1].position)
-		astar.connect_points(nodes.find(connection[0]), nodes.find(connection[1]), true)
-	pathfinding.astar = astar
-	pathfinding.astar_ready = true
+		if !astar.has_point(nodes.find(connection[0])+global_id):
+			astar.add_point(nodes.find(connection[0])+global_id, connection[0].global_position)
+		if !astar.has_point(nodes.find(connection[1])+global_id):
+			astar.add_point(nodes.find(connection[1])+global_id, connection[1].global_position)
+		astar.connect_points(nodes.find(connection[0])+global_id, nodes.find(connection[1])+global_id, true)
+	pathfinding.add_astar(astar)
+	#pathfinding.astar = astar
+	#pathfinding.astar_ready = true
