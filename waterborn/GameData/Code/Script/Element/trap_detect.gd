e00569f8b3cc17669736cf6fafb86c1188b4a095
QUILLIEC-Matisse-24019562 <163044607+QUILLIEC-Matisse-24019562@users.noreply.github.com>
2026-05-22 10:48:07 +0200

    fix warning, and make button work again

--- waterborn/GameData/Code/Script/Element/trap_detect.gd
+++ waterborn/GameData/Code/Script/Element/trap_detect.gd
@@ -3,11 +3,11 @@ extends Area3D
 @export var spawnTab :Array[Node3D]
 #relier le piège a un ou des marker 3D pour faire spawns le monstre 
 
-var their_in = false
-var new_position
-var character
-var monster = preload("res://GameData/Code/Scene/Character/Monster.tscn")
-
+var their_in : bool = false
+var new_position : Vector3
+var character : Node3D
+var distance : float
+var monster_entity : Resource = preload("res://GameData/Code/Scene/Character/Monster.tscn")
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group('player'):
@@ -19,16 +19,12 @@ func _on_body_exited(body: Node3D) -> void:
 	if body.is_in_group('player'):
 		their_in = false
 
-func _physics_process(delta: float) -> void:
-	var distance = 0
+func _physics_process(_delta: float) -> void:
 	if their_in : 
-		if(character.position - new_position).length() > 5 && !get_tree().get_nodes_in_group('enemies'):
+		distance = (character.position - new_position).length()
+		if distance > 5.0 && !get_tree().get_nodes_in_group('enemies'):
 			new_position = character.position
 			var spawn = spawnTab.pick_random()
-			var newMonster = monster.instantiate()
+			var newMonster = monster_entity.instantiate()
 			get_tree().current_scene.add_child(newMonster)
 			newMonster.global_position = spawn.global_position
-
-			
-			
-		
